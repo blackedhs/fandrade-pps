a9a2af8b1a1f76c6053733c10349c27567fab3d6
Andrade Federico <fedepcs@gmail>
2020-08-30 21:39:31 -0300

    login terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./componentes/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./componentes/home/home.module').then( m => m.HomePageModule)
   }
+
+
 ];
 @NgModule({
   imports: [
